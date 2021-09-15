d8dc1ad8776cedc8dbcbd709a440c6130c8d71f3
Giguru Scheuer <giguru@uw-s.nl>
2021-09-15 13:52:29 +0200

    Removed old work from master

--- converse/src/retriever/pipeline_composition_error.py
+++
@@ -1,6 +0,0 @@
-
-
-class PipelineCompositionError(Exception):
-    """
-    Raised if the composition of the pipeline is incorrect.
-    """
--- enums.py
+++
@@ -1,4 +0,0 @@
-
-lang = {
-    "framework": "ConverSE"
-}
